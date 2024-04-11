3c5b9408007e93fb28f064d9221cbf8a913daadc
ann <ann110149@gmail.com>
2024-04-11 21:52:38 +0800

    add usePayment.js composables

---
+++ src/composables/usePaymentCard.js
@@ -0,0 +1,45 @@
+import { ElMessage, ElMessageBox } from 'element-plus'
+import { ref } from 'vue'
+import Decimal from 'decimal.js';
+
+
+export function usePaymentCard(){
+  const messageBox = (detail, success, confirm, callBack) => {
+    ElMessageBox.confirm(detail, '警告', {
+      confirmButtonText: '確定',
+      cancelButtonText: '取消',
+      type: 'warning'
+    })
+      .then((res) => {
+        callBack(res)
+        ElMessage({
+          type: 'success',
+          message: success
+        })
+      })
+      .catch(() => {
+        ElMessage({
+          type: 'info',
+          message: confirm
+        })
+      })
+  }
+	
+	const minusComma = (val) => {
+		let num = val ? val.replace(/,/g, '') * 1 : 0
+		return num
+	}
+
+	const addComma = (val) => {
+		let num = val ? val.toLocaleString('zh', { style: 'decimal' }) : 0
+		return num
+	}
+
+  const percentMethod = (percent, div, times, fixed) => {
+		let num = percent ? new Decimal(percent).div(div).times(times).toFixed(fixed) : 0
+		return num
+	}
+
+
+  return { messageBox, addComma, minusComma, percentMethod }
+}
