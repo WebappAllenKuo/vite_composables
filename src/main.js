dc772ca5444791e045fc2e839559771ece263fde
ann <ann110149@gmail.com>
2024-04-30 21:12:58 +0800

    update: add vue-router

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import { createApp } from 'vue'
-//import './style.css'
-import '@/styles/style.scss'
+import App from './App.vue'
 import ElementPlus from 'element-plus'
 import Decimal from 'decimal.js'
+import router from './router'
+import { useRouter } from 'vue-router'
+import '@/styles/style.scss'
 import 'element-plus/dist/index.css'
 import 'default-passive-events'
-import App from './App.vue'
 
 const app = createApp(App)
 
 app.use(ElementPlus)
+app.use(router)
+app.use(useRouter, router)
 app.config.globalProperties.$decimal = Decimal;
 app.mount('#app')
