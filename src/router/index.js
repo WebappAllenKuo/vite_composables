609a3577e3058f2a8fdc7b545c61407751984950
ann <ann110149@gmail.com>
2024-04-30 21:13:21 +0800

    add: add router/index

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('../views/Home.vue'),
+    redirect: 'composables',
+    children: [
+      {
+        path: 'composables',
+        name: 'Composables',
+        component: () => import('../components/composables.vue'),
+      },
+      {
+        path: 'direactive',
+        name: 'Direactive',
+        component: () => import('../components/directive.vue'),
+      },
+      {
+        path: 'final',
+        name: 'Final',
+        component: () => import('../components/final.vue'),
+      }
+    ]
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
